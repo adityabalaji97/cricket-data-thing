1068e4f1c90f2576f837b3b4d0dafc82c0b96a42
adityabalaji97 <110972021+adityabalaji97@users.noreply.github.com>
2026-01-04 10:16:58 +0530

    Add theme provider and UI primitives

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Analytics } from '@vercel/analytics/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <Analytics />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+      <Analytics />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
